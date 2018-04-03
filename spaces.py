e5e3da359daa98ed197d0ae2028d267e394a8060
Henry Jordan <hank@henryjordan.com>
2018-04-03 09:28:35 -0400

    Implement argparse in spaces.py, code cleanup

--- spaces.py
+++ spaces.py
@@ -1,37 +1,43 @@
 import os, sys
-#import argparse
+import argparse
 
-#May be implemented in a future release
-#parser = argparse.ArgumentParser()
-#parser.parse_args()
+#Initializes argument parser to handle the arguments
+parser = argparse.ArgumentParser(description="Recursively replaces spaces with tabs")
 
-if (len(sys.argv) < 3):
-	print("Too few arguments!")
-	print("")
-	print("usage: spaces.py directory extension")
-	print("spaces.py C:/programming/python/myproject py")
-	sys.exit()
+#Creates arguments for program
+parser.add_argument("directory")
+parser.add_argument("extension")
+parser.add_argument("-q","--quiet",action="store_true",help="toggle quiet mode (no input/output or verification)")
+
+#Shows help if no arguments are entered
+if len(sys.argv)==1:
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+#Finalizes arguments and gets the results
+args = parser.parse_args()
 
 #Gets arguments for directory and extension
-dir=sys.argv[1]	
-ext=sys.argv[2]
+dir=args.directory
+ext=args.extension
 
-#Verifies that the user has entered the correct directory
-verify = input("The directory you chose is "+dir+", is this correct? (y/n): ")
-if (verify != "y"):
-	print("User exited program, quitting!")
-	sys.exit()
+if (not args.quiet):
+	#Verifies that the user has entered the correct directory
+	verify = input("The directory you chose is "+dir+", is this correct? (y/n): ")
+	if (verify != "y"):
+		print("User exited program, quitting!")
+		sys.exit(1)
 
-#Verifies that the user has entered the correct extension
-verify = input("The extension you chose is "+ext+", is this correct? (y/n): ")
-if (verify != "y"):
-	print("User exited program, quitting!")
-	sys.exit()
+	#Verifies that the user has entered the correct extension
+	verify = input("The extension you chose is "+ext+", is this correct? (y/n): ")
+	if (verify != "y"):
+		print("User exited program, quitting!")
+		sys.exit(1)
 
 #Checks if user supplied directory actually exists
 if (not os.path.isdir(dir)):
 	print("Directory invalid, quitting!")
-	sys.exit()
+	sys.exit(1)
 
 #Define spaces and tabs
 spaces = "    "
@@ -46,7 +52,8 @@ for root, dirs, files in os.walk(dir):
 			#Make the full path easier to use and look nice
 			longfile = os.path.join(root, file).replace("\\","/")
 			
-			print("Modifying "+longfile)
+			if (not args.quiet):
+				print("Modifying "+longfile)
 			
 			#Opens file for reading, reads it into a string, replaces the spaces with tabs
 			fr = open(longfile,"r")
